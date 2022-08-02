b122ca26baa8de429ebbe8e55b56f44de300f816
su3ry <110433353+su3ry@users.noreply.github.com>
2022-08-01 17:00:50 -0700

    Add files via upload

---
+++ solver0.py
@@ -0,0 +1,204 @@
+import random
+from itertools import *
+import time
+import numpy as np
+from scipy import optimize
+import sys
+import os
+from task_assignment_algorithm import *
+
+def generate_POI(num_cate, num_POI):
+    POI = []
+    num_per_cate = [0]*num_cate
+    for i in range(num_POI):
+        temp = []
+        for j in range(num_cate):
+            temp.append(random.randint(0, 6))
+            num_per_cate[j] += temp[j]
+        POI.append(temp)
+    return POI, num_per_cate
+def generate_POI_0(num_cate, num_POI, task_per_POI): # In this function, we want every POI has 5 tasks
+    POI = []
+    num_per_cate = np.zeros(num_cate, dtype=int)
+    for i in range(num_POI):
+        temp = np.zeros(num_cate, dtype=int)
+        for j in range(task_per_POI):
+            temp[random.randint(0, num_cate-1)] += 1
+        POI.append(list(temp))
+        num_per_cate = np.add(num_per_cate, temp)
+    return POI, list(num_per_cate)
+
+def next_lambda(compact_form_stra, current_action, POI_cate_matrix, row_actions, agent_cate_matrix, original_capacity):
+    #Use binary search to find a lambda that will give us a new action
+
+    left = 0
+    right = len(current_action) * find_mincomp_act(compact_form_stra, current_action)
+    compact_form = compact_form_stra.copy()
+    A = overlap_POI(row_actions, row_actions)
+    A = np.array(A)
+    temp_v = []
+    new_action = current_action
+    for act in row_actions:
+        temp_v.append(normal_action_value(compact_form_stra, act))
+
+    while (right - left > 0.01):
+        temp = (left+right)/2
+        B = np.array(temp_v)-np.array([temp]*len(row_actions))
+
+        #B = np.array([temp]*len(row_actions))
+        sol = optimize.linprog(np.array([1]*len(row_actions)), A_ub = np.multiply(A, -1), b_ub = np.multiply(B, -1))
+        x = sol.get('x')
+        
+        compact_form = compact_form_stra.copy()
+        for action in range(len(row_actions)):
+            for poi in row_actions[action]:
+                compact_form[poi] = compact_form[poi] - x[action]
+        current_row_act = max_normal_form_action(POI_cate_matrix, compact_form, agent_cate_matrix, original_capacity, list(range(agent_num)))
+
+        #current_v = normal_action_value(compact_form_stra, current_action)
+        #next_v = normal_action_value(compact_form_stra, next_action)
+        if overlap_POI([current_row_act], [current_action]) == [[len(current_action)]]:
+            left = temp
+        else:
+            new_action = current_row_act
+            right = temp
+
+
+    B = np.array(temp_v)-np.array([right]*len(row_actions))
+
+    sol = optimize.linprog(np.array([1]*len(row_actions)), A_ub = np.multiply(A, -1), b_ub = np.multiply(B, -1))
+    x = sol.get('x')
+    compact_form = compact_form_stra.copy()
+    for action in range(len(row_actions)):
+        for poi in row_actions[action]:
+            compact_form[poi] = compact_form[poi] - x[action]
+    row_actions.append(new_action)
+    return right, compact_form, x
+
+def normal_action_value(compact_form_stra, normal_form_action):
+    v = 0
+    for poi in normal_form_action:
+        v += compact_form_stra[poi]
+    return v
+
+def update(compact_form, row_actions, x):
+    updated_c = compact_form.copy()
+    for acti in range(len(row_actions)):
+        for i in row_actions[acti]:
+            updated_c[i] -= x[acti]
+    return updated_c
+
+def homotopy(POI_cate_matrix, compact_form_stra, agent_cate_matrix, original_capacity):
+    
+    row_actions = []
+    lam = []
+    agent_num = len(agent_cate_matrix)
+    cate_num = len(agent_cate_matrix[0])
+    POI_num = len(POI_cate_matrix)
+    prob_vector = compact_form_stra.copy()
+    ########  Intialization ########
+    current_row_act = max_normal_form_action(POI_cate_matrix, prob_vector, agent_cate_matrix, original_capacity, list(range(agent_num)))
+    current_value = normal_action_value(prob_vector, current_row_act)
+    print(prob_vector, current_row_act)
+    #####################################
+
+    row_actions.append(current_row_act)
+    current_lam = current_value - 0.01
+    lam.append(current_lam)
+    A = overlap_POI(row_actions, row_actions)
+    A = np.array(A)
+    A_dual = np.transpose(A)
+    temp_v = []
+    for act in row_actions:
+        temp_v.append(normal_action_value(compact_form_stra, act))
+    B = np.array(temp_v)-np.array([current_lam]*len(row_actions))
+    sol = optimize.linprog(np.array([1]*len(row_actions)), A_ub = np.multiply(A, -1), b_ub = np.multiply(B, -1))
+    x = sol.get('x')
+
+    prob_vector = update(compact_form_stra, row_actions, x)
+    #print("prob_vector: ", prob_vector)
+    #print("compact form stra: ", compact_form_stra)
+    #print(row_actions)
+    while (len(lam) == 0 or lam[-1] >= 0.1):
+        
+        #############################################################
+        current_row_act = max_normal_form_action(POI_cate_matrix, prob_vector, agent_cate_matrix, original_capacity, list(range(agent_num)))
+        current_value = normal_action_value(prob_vector, current_row_act)
+        #############################################################
+        in_row_actions = False
+
+        ###########################################################
+        if in_row_actions == False:
+            current_lam = current_value-0.01
+            lam.append(current_lam)
+            if sorted(current_row_act) not in row_actions:
+                row_actions.append(sorted(current_row_act))
+
+            A = overlap_POI(row_actions, row_actions)
+            A = np.array(A)
+            A_dual = np.transpose(A)
+            temp_v = []
+            for act in row_actions:
+                temp_v.append(normal_action_value(compact_form_stra, act))
+            B = np.array(temp_v)-np.array([current_lam]*len(row_actions))
+            sol = optimize.linprog(np.array([1]*len(row_actions)), A_ub = np.multiply(A, -1), b_ub = np.multiply(B, -1))
+            x = sol.get('x')
+            prob_vector = update(compact_form_stra, row_actions, x)
+        print(lam[-1])
+
+    return row_actions, x, lam
+       
+def overlap_POI(row_actions, column_actions):
+    A = []
+    for i in range(len(row_actions)):
+        temp = []
+        for j in range(len(column_actions)):
+            intersection = [p for p in row_actions[i] if p in column_actions[j]]
+            temp.append(len(intersection))
+        A.append(temp)
+    return A
+
+def convert_matrix(agents_time_matrix, action): #action here is in the form of task category
+    task_agent_time_matrix = np.zeros((sum(action), len(agents_time_matrix)), dtype = int)
+    i = 0
+    for cate in range(len(action)):
+        for j in range(i, i+action[cate]):
+            for agent in range(len(agents_time_matrix)):
+                task_agent_time_matrix[j][agent] = agents_time_matrix[agent][cate]
+        i += action[cate]
+    return task_agent_time_matrix.tolist()
+
+
+if __name__ == "__main__":
+    cate_num = 3
+    agent_num = 10
+    POI_num = 30
+    task_per_POI = 8
+    homotopy_time_list = []
+    repeat_time = 1
+    for POI_num in range(10, 11, 2):
+        time_bound = int(5*POI_num/2)
+        temp_time = 0
+        for fre in range(repeat_time):
+            POI_cate_matrix, num_per_cate = generate_POI_0(cate_num, POI_num, task_per_POI)
+            POI_cate_matrix = np.array(POI_cate_matrix)
+            print(POI_cate_matrix)
+            agent_cate_matrix = np.zeros((agent_num, cate_num), dtype=int)
+            compact_form_stra = []
+            for i in range(agent_num):
+                for j in range(cate_num):
+                    agent_cate_matrix[i][j] = random.randint(1, 10)
+            original_capacity = [time_bound] * agent_num
+            for i in range(POI_num):
+                compact_form_stra.append(random.random())
+            t1 = time.time()
+            valid_actions, x, lam = homotopy(POI_cate_matrix, compact_form_stra, agent_cate_matrix, original_capacity)
+            t2 = time.time()
+            print("valid_actions:", valid_actions)
+            print("solution:", x)
+            print("number of tasks per category: ", num_per_cate)
+            print("lam:", lam)
+            print("Homotopy time: ", t2-t1)
+            temp_time += (t2-t1)
+        homotopy_time_list.append(temp_time/repeat_time)
+    print(homotopy_time_list)
---
+++ task_assignment_algorithm.py
@@ -0,0 +1,379 @@
+import numpy as np
+
+import random
+import sys
+
+## Input: 1. probability vector
+###       2. POI_num * cate_num matrix, each entry the number of tasks in POI i of category j
+###       3. Agent_num * cate_num time matrix, each entry the time agent i needed for category j task
+###       Note i is the row index, and j is the column index
+
+def dynamic_programming(values, weights, capacity): ## This is a subprotocol for GAP (generalized assignment problem), where we assign every agent at one time
+    ## Note that wieghts and capacity must be integer, but values can be floats
+    #print(capacity) 
+    if len(values) == 1:
+        if weights[0] <= capacity:
+            return values[0], np.array([1])
+        else:
+            return 0, np.array([0])
+    if capacity == 0:
+        return 0, np.array([0]*len(values))
+    n = len(values)
+    assignments = np.zeros((n, capacity, n), dtype = int)
+    matrix = np.zeros((n, capacity))
+    temp0 = np.zeros(n, dtype = int)
+    temp0[0] = 1
+    for j in range(1, capacity):
+        if weights[0] <= j:
+            matrix[0][j] = values[0]
+            assignments[0][j] = temp0
+    for i in range(1, n):
+        for j in range(1, capacity):
+            if weights[i] > j:
+                matrix[i][j] = matrix[i-1][j]
+                assignments[i][j]  = assignments[i-1][j]
+
+            else:
+                if matrix[i-1][j]> (matrix[i-1][j-weights[i]]+values[i]):
+                    matrix[i][j] = matrix[i-1][j]
+                    assignments[i][j] = assignments[i-1][j]
+                else:
+                    temp = assignments[i-1][j-weights[i]]
+                    temp[i] = 1
+                    assignments[i][j] = temp
+                    matrix[i][j] = matrix[i-1][j-weights[i]]+values[i]
+    #print(n, capacity)
+    return matrix[n-1][capacity-1], assignments[n-1][capacity-1]
+
+def generate_weights_values(n):
+    temp1 = []
+    temp2 = []
+    for i in range(n):
+        temp1.append(random.randint(1, 10))
+        temp2.append(random.random())
+
+    return temp1, temp2
+
+
+def utility_matrix_fixed(POI_cate_matrix, prob_vector):
+    # We fix each task utility with the value of p divided by the total number of tasks in that POI
+    POI_num = len(POI_cate_matrix)
+    cate_num = len(POI_cate_matrix[0])
+    utility_matrix = np.zeros((POI_num, cate_num))
+    for i in range(POI_num):
+        temp = POI_cate_matrix[i].sum()
+        for j in range(cate_num):
+            if POI_cate_matrix[i][j] > 0:
+                utility_matrix[i][j] = prob_vector[i]/temp
+    return utility_matrix
+
+def convert_cate_to_individual(unassigned_matrix, utility_matrix, weight_cate_vector):
+    ## Weight cate vector is a vecor of an agent's time to complete different category of tasks
+    POI_num = len(unassigned_matrix)
+    cate_num = len(unassigned_matrix[0])
+    weights = []
+    values = []
+    for i in range(POI_num):
+        for j in range(cate_num):
+            for k in range(unassigned_matrix[i][j]):
+                weights.append(weight_cate_vector[j])
+                values.append(utility_matrix[i][j])
+    return weights, values
+
+def reverse_indi_to_cate_assign(indi_assignment, unassigned_matrix):
+    # we need to return a matrix in the following format and return a new unassigned_matrix:
+        #  [Agent_0:        task_cate_0   ...   task_cate_n
+        #           POI_i1 [[                               ],
+        #           ...     [                               ],
+        #           POI_im  [                               ]],
+        #
+        #   ...
+        #
+        #   Agent_p:        task_cate_0   ...   task_cate_n
+        #           POI_i1 [[                               ],
+        #           ...     [                               ],
+        #           POI_im  [                               ]],
+        
+
+        if len(indi_assignment) != unassigned_matrix.sum():
+            print(indi_assignment)
+            print(unassigned_matrix)
+            print("Unmatched individual assignment and unassigned matrix!")
+            sys.exit(1)
+        n = len(indi_assignment)
+        POI_num = len(unassigned_matrix)
+        cate_num = len(unassigned_matrix[0])
+        assign_matrix = np.zeros((POI_num, cate_num), dtype=int)
+        row = 0
+        column = 0
+        current = 0
+        for i in range(POI_num):
+            for j in range(cate_num):
+                if unassigned_matrix[i][j] != 0:
+                    l = unassigned_matrix[i][j]
+                    for k in range(current, current+l):
+                        if indi_assignment[k] == 1:
+                            assign_matrix[i][j] += 1
+                            unassigned_matrix[i][j] -= 1
+                    current += l
+        return assign_matrix
+                
+
+            
+def transfer(assignment, POI_cate_matrix, unassigned_matrix, capacity, agent_cate_matrix):
+    # We need transfer the assignment to the final_assignment which will give a betther coverage of more POIs
+    # Assignment is the 3D-matrix, and final_assignment should also a 3D-matrix
+    agent_num = len(assignment)
+    POI_num = len(assignment[0])
+    cate_num = len(assignment[0][0])
+    coverage = np.zeros((POI_num, cate_num), dtype = int)
+    for j in range(POI_num):
+        temp = np.zeros(cate_num, dtype = int)
+        for i in range(agent_num):
+            temp += assignment[i][j]
+        coverage[j] = temp
+        if (np.array(POI_cate_matrix[j])-temp).sum() > 1: # 5 is a value we could change
+            for i in range(agent_num):
+                #assignment[i][j] = np.zeros(cate_num, dtype = int)
+                for k in range(cate_num):
+                    unassigned_matrix[j][k] += assignment[i][j][k]
+                assignment[i][j] = np.zeros(cate_num, dtype = int)
+        else:
+            for i in range(agent_num):
+                for k in range(cate_num):
+                    capacity[i] -= (assignment[i][j][k] * agent_cate_matrix[i][k])
+
+    return coverage
+
+def check_POI_coverage(assignment, POI_cate_matrix):
+    count = 0
+    POI_covered = []
+    agent_num = len(assignment)
+    POI_num = len(POI_cate_matrix)
+    cate_num = len(POI_cate_matrix[0])
+    for j in range(POI_num):
+        temp = np.zeros(cate_num, dtype = int)
+        for i in range(agent_num):
+            temp += assignment[i][j]
+        if (np.array(POI_cate_matrix[j] - temp).sum() == 0):
+            count += 1
+            POI_covered.append(j)
+    return POI_covered
+
+def compute_slack(assignment, capacity, agent_cate_matrix):
+    # Return a vector of time slack for every agent after the assignment
+
+    agent_num = len(agent_cate_matrix)
+    temp = capacity.copy()
+    slack= np.array(temp)
+    cate_num = len(agent_cate_matrix[0])
+    POI_num = len(assignment[0])
+    for i in range(agent_num):
+        for j in range(POI_num):
+            for k in range(cate_num):
+                slack[i] -= (assignment[i][j][k] * agent_cate_matrix[i][k])
+    return slack
+        
+def check_correctness(final_assignment, agent_cate_matrix):
+    ch = []
+    agent_num = len(final_assignment)
+    POI_num = len(final_assignment[0])
+    cate_num = len(agent_cate_matrix[0])
+    for ag in range(agent_num):
+        a1 = np.reshape(final_assignment[ag], POI_num*cate_num)
+        a2 = np.array(list(agent_cate_matrix[ag])*POI_num)
+        ch.append(np.dot(a1,a2))
+    return ch
+
+def divide_assignment(assignment, covered_POI, POI):
+    # Divide the 3-d asssignment into two 3d matrix, one is the fixed one which represent those with POI covered, the other is the unfiexed one
+
+    agent_num = len(assignment)
+    cate_num = len(assignment[0][0])
+    POI_num = len(assignment[0])
+    fixed_assignment = np.zeros((agent_num, POI_num, cate_num), dtype = int)
+    for j in range(POI_num):
+        if j in covered_POI or j == POI:
+            for i in range(agent_num):
+                fixed_assignment[i][j] = assignment[i][j]
+
+    unfixed_assignment = assignment-fixed_assignment
+    return fixed_assignment, unfixed_assignment
+
+def reassign_value(fixed_assignment, unfixed_assignment, slack, POI, agent_cate_matrix, utility_matrix, POI_cate_matrix, prob_vector):
+    # This returns the largest reassign value for the POI, and its corresponding assignment
+    agent_num = len(agent_cate_matrix)
+    cate_num = len(agent_cate_matrix[0])
+    POI_num = len(POI_cate_matrix)
+    assigned_tasks = np.zeros(cate_num, dtype = int)
+    task_utility = prob_vector[POI]/(POI_cate_matrix[POI].sum())
+    for i in range(agent_num):
+        assigned_tasks += fixed_assignment[i][POI]
+    unassigned_tasks = POI_cate_matrix[POI] - assigned_tasks
+    total_decreased_value = 0
+    
+    for i in range(cate_num):
+        if unassigned_tasks[i] > 0:
+            for j in range(unassigned_tasks[i]):
+                decreased_values = []
+                assigns = []
+                for k in range(agent_num):
+                    lower_bound = agent_cate_matrix[k][i]
+                    temp_weights = []
+                    temp_values = []
+                    for j1 in range(POI_num):
+                        for l in range(cate_num):
+                            if unfixed_assignment[k][j1][l] > 0:
+                                for n in range(unfixed_assignment[k][j1][l]):
+                                    temp_weights.append(agent_cate_matrix[k][l])
+                                    temp_values.append(utility_matrix[j1][l])
+                    if len(temp_weights) == 0:
+                        if slack[k] >= agent_cate_matrix[k][i]:
+                            decreased_values.append(0)
+                            assigns.append([])
+                        else:
+                            decreased_values.append(10000)
+                            assigns.append([])
+                        continue
+                    lower_bound -= slack[k]
+                    if lower_bound <= 0:
+                        decreased_value = 0
+                        temp_assign = np.zeros(len(temp_weights), dtype = int)
+                    else:
+                        decreased_value, temp_assign = minimize_decreased_values(temp_weights, temp_values, lower_bound)
+                    decreased_values.append(decreased_value)
+                    assigns.append(temp_assign)
+                    mu = np.multiply(temp_assign, temp_weights).sum()
+                temp_min = min(decreased_values)
+                if temp_min > 1000:
+                    return 1, 0
+                ind = decreased_values.index(temp_min)
+                temp_unassign = assigns[ind]
+                total_decreased_value -= temp_min
+                total_decreased_value += task_utility
+                #print(temp_min, task_utility)
+                unassign_matrix = reverse_indi_to_cate_assign(temp_unassign, unfixed_assignment[ind])
+                
+                #cate_pos = np.where(unassign_matrix == 1)[-1][0]
+                original_slack = slack[ind]
+                for p0 in range(POI_num):
+                    for c0 in range(cate_num):
+                        if unassign_matrix[p0][c0] > 0:
+                            slack[ind] += (agent_cate_matrix[ind][c0] * unassign_matrix[p0][c0])
+                slack[ind] -= agent_cate_matrix[ind][i]
+                if slack[ind] < 0:
+                    print(agent_cate_matrix[ind])
+                    print(original_slack, agent_cate_matrix[ind][i])
+                    print(unassign_matrix)
+                    
+                    print("slack: ", slack[ind])
+                    sys.exit(1)
+                fixed_assignment[ind][POI][i] += 1
+    return 0, total_decreased_value
+                
+                    
+
+def minimize_decreased_values(weights, values, lower_bound):
+    n = len(values)
+    #print(n, lower_bound)
+    if n == 1:
+        if lower_bound < weights[0]:
+            return values[0], np.array([1])
+        else:
+            return 10000, np.array([0])
+    assignments = np.zeros((n, lower_bound+1, n), dtype = int)
+    matrix = np.full((n, lower_bound+1), 10000, dtype = float)
+    for i in range(n):
+        for j in range(lower_bound+1):
+            if weights[i] >= j:
+                if i == 0:
+                    matrix[i][j] = values[i]
+                    temp = np.zeros(n, dtype = int)
+                    temp[0] = 1
+                    assignments[i][j] = temp
+                else:
+                    matrix[i][j] = min(matrix[i-1][j], values[i])
+                    if matrix[i][j] == matrix[i-1][j]:
+                        assignments[i][j] = assignments[i-1][j]
+                    else:
+                        temp = np.zeros(n, dtype = int)
+                        temp[i] = 1
+                        assignments[i][j] = temp
+            else:
+
+                if matrix[i-1][j] > (matrix[i-1][j-weights[i]]+values[i]):
+                    matrix[i][j] = matrix[i-1][j-weights[i]]+values[i]
+                    temp = assignments[i-1][j-weights[i]]
+                    temp[i] = 1
+                    assignments[i][j] = temp
+                elif matrix[i-1][j] <= (matrix[i-1][j-weights[i]]+values[i]):
+                    matrix[i][j] = matrix[i-1][j]
+                    assignments[i][j] = assignments[i-1][j]
+    
+    return matrix[n-1][lower_bound], assignments[n-1][lower_bound]
+
+def assign(unassigned_matrix, prob_vector, agent_cate_matrix, capacity, agent_order, utility_matrix):
+    
+    agent_num = len(agent_cate_matrix)
+    POI_num = len(unassigned_matrix)
+    cate_num = len(unassigned_matrix[0])
+    final_assignment = np.zeros((agent_num, POI_num, cate_num), dtype = int)
+    for i in agent_order:
+        if unassigned_matrix.sum() == 0:
+            break
+        #utility_matrix = utility_matrix_conversion(unassigned_matrix, prob_vector)
+        
+        weights, values = convert_cate_to_individual(unassigned_matrix, utility_matrix, agent_cate_matrix[i])
+        if len(weights) != unassigned_matrix.sum():
+            print("Unmatched length!")
+            sys.exit(1)
+        max_value,indi_assignment = dynamic_programming(values, weights, capacity[i])
+        assign_matrix = reverse_indi_to_cate_assign(indi_assignment, unassigned_matrix)
+        final_assignment[i] = assign_matrix
+
+    return final_assignment
+
+
+
+def max_normal_form_action(POI_cate_matrix, prob_vector, agent_cate_matrix, original_capacity, agent_order):
+    agent_num = len(agent_cate_matrix)
+    cate_num = len(agent_cate_matrix[0])
+    POI_num = len(prob_vector)
+    utility_matrix = utility_matrix_fixed(POI_cate_matrix, prob_vector)
+    unassigned_matrix = POI_cate_matrix.copy()
+    capacity = original_capacity.copy()
+    final_assignment = assign(unassigned_matrix, prob_vector, agent_cate_matrix, capacity, list(range(agent_num)), utility_matrix)
+
+    capacity = original_capacity.copy()
+    covered_POI = check_POI_coverage(final_assignment, POI_cate_matrix)
+    slack = compute_slack(final_assignment, capacity, agent_cate_matrix)
+    while True:
+        unused_POI = []
+        candidate_POI = []
+        all_decreased_values = []
+        fixed_assignments = []
+        unfixed_assignments = []
+        for i in range(POI_num):
+            if i not in covered_POI:
+                #print(i)
+                fixed_assignment, unfixed_assignment = divide_assignment(final_assignment, covered_POI, i)
+                capacity = original_capacity.copy()
+                slack = compute_slack(final_assignment, capacity, agent_cate_matrix)
+
+                indicator, total_decreased_value = reassign_value(fixed_assignment, unfixed_assignment, slack, i, agent_cate_matrix, utility_matrix, POI_cate_matrix, prob_vector)
+                if indicator == 1:
+                    unused_POI.append(i)
+                else:
+                    candidate_POI.append(i)
+                    all_decreased_values.append(total_decreased_value)
+                    fixed_assignments.append(fixed_assignment)
+                    unfixed_assignments.append(unfixed_assignment)
+        if len(candidate_POI) == 0:
+            break
+        max_update = max(all_decreased_values)
+        max_ind = all_decreased_values.index(max_update)
+        unfixed_assignment = unfixed_assignments[max_ind]
+        fixed_assignment = fixed_assignments[max_ind]
+        final_assignment = fixed_assignment + unfixed_assignment
+        covered_POI.append(candidate_POI[max_ind])
+    return covered_POI
